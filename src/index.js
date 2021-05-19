8c83b3df565535bcd8b00870abe2aac5d5986a12
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-05-19 13:29:42 +0200

    Add files in scene folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import 'phaser';
 import config from './config/config';
 import GameScene from './scenes/games-scenes';
+import BootScene from './scenes/boot-scene';
+import PreloaderScene from './scenes/preloader-scene';
+import TitleScene from './scenes/title-scene';
+import OptionsScene from './scenes/options-scene';
+import CreditsScene from './scenes/credits-scene';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
     this.scene.start('Game');
   }
