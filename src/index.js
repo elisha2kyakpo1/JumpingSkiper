cfa830568cbe76b6cef7a1ec7b1e12e00747e986
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-05-21 17:11:07 +0200

    Ressolve module mismatch

--- src/index.js
+++ src/index.js
@@ -11,16 +11,44 @@ import Model from './model';
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    this.scene.add('Game', GameScene);
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Options', OptionsScene);
     this.scene.add('Credits', CreditsScene);
-    this.scene.add('Game', GameScene);
     this.scene.start('Game');
     const model = new Model();
     this.globals = { model, bgMusic: null };
   }
 }
 
-window.game = new Game();
+const Config = {
+  type: Phaser.AUTO,
+  parent: 'content',
+  width: 640,
+  height: 640,
+  zoom: 1,
+  dom: {
+    createContainer: true,
+  },
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
+  scene: [
+    BootScene,
+    GameScene,
+    PreloaderScene,
+    TitleScene,
+    CreditsScene,
+    OptionsScene,
+  ],
+};
+
+// eslint-disable-next-line no-new
+// new Phaser.Game(gameConfig);
+window.game = new Game(Config);
